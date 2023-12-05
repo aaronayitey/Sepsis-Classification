c52aafcd2d32a7654305cdbd6784adf94714dd91
Aaron Ayitey <aaronayitey@gmail.com>
2023-12-05 16:40:49 +0000

    Make changes

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ app = FastAPI()
 
 
 class features(BaseModel):
-    Plasma_Glucose: float
-    Elevated_Glucose: float
-    Diastolic_Blood_Pressure: float
-    Triceps_Skinfold_Thickness: float
-    Insulin_Levels: float
-    Body_Mass_Index_BMI: float
-    Diabetes_Pedigree_Function: float
     Age: int
+    Body_Mass_Index_BMI: float
+    Diastolic_Blood_Pressure: float
+    Plasma_Glucose: float
+    Triceps_Skinfold_Thickness: float
+    Elevated_Glucose: float
+    Diabetes_Pedigree_Function: float
+    Insulin_Levels: float
 
 
 @app.post("/predict")
@@ -27,9 +27,6 @@ async def predict_sepsis(item: features):
     try:
         # Convert input data to DataFrame
         input_data = pd.DataFrame([item.dict()])
-        
-        # Print input_data for debugging
-        print("Input Data:", input_data)
 
         input_data = pipeline.named_steps.preprocessor.transform(input_data)
 
@@ -42,6 +39,4 @@ async def predict_sepsis(item: features):
         return {"prediction": f'Patient is {decoded_predictions[0]}'}
 
     except Exception as e:
-        print("Error:", str(e))
         raise HTTPException(status_code=500, detail=str(e))
-
